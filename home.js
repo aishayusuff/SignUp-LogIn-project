7a9b35b1d6e74c41f4967f47efca4291ed2dde5a
aishayusuff <aishatyusuffe@gmail.com>
2022-07-09 21:40:25 +0100

    Mobile responsiveness
    Some minor changes to make this project responsive to mobile devices.

--- home.js
+++ home.js
@@ -1,5 +1,7 @@
 const signup_btn = document.querySelector("#signup-btn");
 const login_btn = document.querySelector("#login-btn");
+const mobile_signup_btn = document.querySelector("#mobile-signup");
+const mobile_login_btn = document.querySelector("#mobile-login");
 const container = document.querySelector(".container");
 
 signup_btn.addEventListener("click", (e) => {
@@ -8,4 +10,11 @@ signup_btn.addEventListener("click", (e) => {
 
 login_btn.addEventListener("click", (e) => {
   container.classList.remove("signup-mode");
+});
+mobile_signup_btn.addEventListener("click", (e)=>{
+  container.classList.add("mobile-signup-mode");
+});
+
+mobile_login_btn.addEventListener("click", (e)=>{
+  container.classList.remove("mobile-signup-mode");
 });
